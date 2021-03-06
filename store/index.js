bf601299a25571b7f95e79269c1fba4750f7ada3
Sean Kerwin <s.kerwin@tshirtandsons.com>
2021-03-06 13:30:06 +0000

    first commit

---
+++ store/index.js
@@ -0,0 +1,28 @@
+export const state = () => ({
+  articles: [],
+})
+
+export const mutations = {
+  SET_ARTICLES(state, payload) {
+    state.articles = payload
+  },
+}
+
+export const actions = {
+  nuxtServerInit({ commit }, context) {
+    return context.app.$axios
+      .$get('https://dev.to/api/articles?username=seankerwin')
+      .then((data) => {
+        commit('SET_ARTICLES', data)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  },
+}
+
+export const getters = {
+  articles(state) {
+    return state.articles
+  },
+}
